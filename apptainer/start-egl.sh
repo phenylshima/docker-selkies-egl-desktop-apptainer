2205cb22ead492f6a2dc3ecc2a7f1647279ccca5
phenylshima <49227365+phenylshima@users.noreply.github.com>
2026-02-03 15:42:05 +0900

    change paths

--- apptainer/start-egl.sh
+++ apptainer/start-egl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SINGULARITY_SELKIES_SCRATCH_HOME="images/egl-home"
+export SINGULARITY_SELKIES_SCRATCH_HOME="egl-home"
 mkdir -pm755 "${SINGULARITY_SELKIES_SCRATCH_HOME}"
 
 export SINGULARITY_SELKIES_LOGS_DIR="images/logs"
@@ -9,11 +9,12 @@ mkdir -pm755 "${SINGULARITY_SELKIES_LOGS_DIR}"
 
 mkdir -pm755 /tmp/docker-nvidia-egl-desktop-tmp
 
-singularity shell --nv \
+singularity exec --nv \
   --pid \
   --no-mount cwd,tmp \
   --bind /tmp/docker-nvidia-egl-desktop-tmp:/tmp \
   --bind "${SINGULARITY_SELKIES_LOGS_DIR}:/var/log/" \
   --home "${SINGULARITY_SELKIES_SCRATCH_HOME}:/home/ubuntu" \
   --env "TZ=UTC,DISPLAY_SIZEW=1920,DISPLAY_SIZEH=1080,DISPLAY_REFRESH=60,DISPLAY_DPI=96,DISPLAY_CDEPTH=24,PASSWD=mypasswd,SELKIES_ENCODER=nvh264enc,SELKIES_VIDEO_BITRATE=8000,SELKIES_FRAMERATE=60,SELKIES_AUDIO_BITRATE=128000,SELKIES_BASIC_AUTH_PASSWORD=lithium" \
-  images/docker-nvidia-egl-desktop-nonroot.sif
+  images/docker-nvidia-egl-desktop-nonroot.sif \
+  /usr/bin/supervisord
