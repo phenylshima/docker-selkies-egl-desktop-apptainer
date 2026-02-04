b9ad0400023d21158e00a51912491bf64127405e
phenylshima <49227365+phenylshima@users.noreply.github.com>
2026-02-04 10:38:41 +0900

    more support for multiple instance

--- apptainer/start-egl.sh
+++ apptainer/start-egl.sh
@@ -7,12 +7,19 @@ SINGULARITY_SELKIES_LOGS_DIR="images/logs"
 rm "${SINGULARITY_SELKIES_LOGS_DIR}"/*.log 2> /dev/null || echo 'No existing log files to remove'
 mkdir -pm755 "${SINGULARITY_SELKIES_LOGS_DIR}"
 
-mkdir -pm755 /tmp/docker-nvidia-egl-desktop-tmp
-
 NGINX_PORT=7000
 SELKIES_PORT=8081
 SELKIES_METRICS_HTTP_PORT=9081
-SELKIES_TURN_PORT=3478
+SELKIES_TURN_PORT=2000
+SELKIES_TURN_HOST="127.0.0.1"
+TURN_EXTERNAL_IP="127.0.0.1"
+
+TMP_DIR="/tmp/docker-nvidia-egl-desktop-$(date +%Y%m%d%H%M%S)-$(tr -dc 'A-Za-z0-9' < /dev/urandom 2>/dev/null | head -c 24)"
+if [ -d "${TMP_DIR}" ]; then
+  echo "Error: Temporary directory ${TMP_DIR} already exists. Exiting to prevent conflicts."
+  exit 1
+fi
+mkdir -pm755 "${TMP_DIR}"
 
 singularity exec --nv \
   --pid \
@@ -21,10 +28,14 @@ singularity exec --nv \
   --env SELKIES_PORT=${SELKIES_PORT} \
   --env SELKIES_METRICS_HTTP_PORT=${SELKIES_METRICS_HTTP_PORT} \
   --env SELKIES_TURN_PORT=${SELKIES_TURN_PORT} \
+  --env SELKIES_TURN_HOST=${SELKIES_TURN_HOST} \
+  --env TURN_EXTERNAL_IP=${TURN_EXTERNAL_IP} \
   --no-mount cwd,tmp \
-  --bind /tmp/docker-nvidia-egl-desktop-tmp:/tmp \
+  --bind "${TMP_DIR}:/tmp" \
   --bind "${SINGULARITY_SELKIES_LOGS_DIR}:/var/log/" \
   --home "${SINGULARITY_SELKIES_SCRATCH_HOME}:/home/ubuntu" \
   --env "TZ=UTC,DISPLAY_SIZEW=1920,DISPLAY_SIZEH=1080,DISPLAY_REFRESH=60,DISPLAY_DPI=96,DISPLAY_CDEPTH=24,PASSWD=mypasswd,SELKIES_ENCODER=nvh264enc,SELKIES_VIDEO_BITRATE=8000,SELKIES_FRAMERATE=60,SELKIES_AUDIO_BITRATE=128000,SELKIES_BASIC_AUTH_PASSWORD=lithium" \
   images/docker-nvidia-egl-desktop-nonroot.sif \
   /usr/bin/supervisord
+
+rm -r "${TMP_DIR}"
